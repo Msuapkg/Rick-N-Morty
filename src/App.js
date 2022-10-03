b67df498f40fbc0f4694609618395f67ec9c541a
msuapkg <msuapkg@gmail.com>
2022-10-03 12:51:17 -0500

    First Conection with Rick & Morty API

--- src/App.js
+++ src/App.js
@@ -1,29 +1,58 @@
-import React from 'react';
-import {useSelector} from 'react-redux';
-import { Counter } from './features/counter/Counter';
-import InfoCard from './components/InfoCard/InfoCard';
+import React, {useState} from 'react';
+//import {useSelector} from 'react-redux';
+import InfoCard from './components/InfoCard';
 
 import './App.css';
+import axios from 'axios';
 
 function App() {
 
-  const infoCardState = useSelector(state => state.infoCard);
-  console.log(infoCardState);
+  const [inputValue, setInputValue] = useState('');
 
-  const handleInput = function () {
-    
+  const handleChange = (e) => {
+    setInputValue(e.target.value);
   }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const url = `https://rickandmortyapi.com/api/character/?name=${inputValue}`;
+    console.log(url)
+    console.log(inputValue);
+
+    try {
+      const axiosRes = axios.get(url)
+      .then(response => {
+        console.log(response.data, response.status);
+      }) .catch (error => {
+        console.log(error.response.status, error.response.data);
+      });
+      console.log(axiosRes);
+      
+    } catch (error) {
+      console.log(error, 'Algo salió mal');
+    }
+
+  }
+
 
   return (
     <div className="App">
       <div className='title-app'>
         <h1>Busca tu personaje favorito de Rick & Morty</h1>
       </div>
+      
       <div className='container-input-app'>
-        <input className='input-app'  />
-        <button className='button-app'>Search</button>
+        <form onSubmit={e => handleSubmit(e)}>
+          <input  
+            className='input-app' 
+            type='text' name='Search' 
+            value={inputValue} onChange={ e => handleChange(e) }
+            />
+          <button className='button-app'>Search</button>
+        </form>
       </div>
       <div className='body-app'>
+        {}
         <InfoCard />
       </div>
     </div>
