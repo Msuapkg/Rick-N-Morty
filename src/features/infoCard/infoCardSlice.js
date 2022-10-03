3325dc0f914d5851b09c54449d9ae3cfd06859c5
msuapkg <msuapkg@gmail.com>
2022-10-03 10:21:32 -0500

    Create Card and its actions - slice

---
+++ src/features/infoCard/infoCardSlice.js
@@ -0,0 +1,11 @@
+import {createSlice} from '@reduxjs/toolkit';
+
+const infoCardSlice = createSlice({
+    name: 'info-card',
+    initialState: [],
+    reducers: {
+
+    }
+});
+
+export default infoCardSlice.reducer;
