c9060ac46008150e2830817b739761af76bfccb5
msuapkg <msuapkg@gmail.com>
2022-10-04 04:02:38 -0500

    Create App file

--- src/App.js
+++ src/App.js
@@ -1,60 +1,19 @@
-import React, {useState} from 'react';
-//import {useSelector} from 'react-redux';
+import {useSelector} from 'react-redux';
 import InfoCard from './components/InfoCard';
+import InfoForm from './components/InfoForm'
 
 import './App.css';
-import axios from 'axios';
 
 function App() {
-
-  const [inputValue, setInputValue] = useState('');
-
-  const handleChange = (e) => {
-    setInputValue(e.target.value);
-  }
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const url = `https://rickandmortyapi.com/api/character/?name=${inputValue}`;
-    console.log(url)
-    console.log(inputValue);
-
-    try {
-      const axiosRes = axios.get(url)
-      .then(response => {
-        console.log(response.data, response.status);
-      }) .catch (error => {
-        console.log(error.response.status, error.response.data);
-      });
-      console.log(axiosRes);
-      
-    } catch (error) {
-      console.log(error, 'Algo salió mal');
-    }
-
-  }
-
 
   return (
     <div className="App">
       <div className='title-app'>
-        <h1>Busca tu personaje favorito de Rick & Morty</h1>
+          <h1>Busca tu personaje favorito de Rick & Morty</h1>
+          <img className='gif' src='https://res.cloudinary.com/jerrick/image/upload/v1574029200/5dd1c7902e40ff00232f384a.gif'></img>
       </div>
-      
-      <div className='container-input-app'>
-        <form onSubmit={e => handleSubmit(e)}>
-          <input  
-            className='input-app' 
-            type='text' name='Search' 
-            value={inputValue} onChange={ e => handleChange(e) }
-            />
-          <button className='button-app'>Search</button>
-        </form>
-      </div>
-      <div className='body-app'>
-        {}
-        <InfoCard />
-      </div>
+      <InfoForm />
+      <InfoCard/>
     </div>
   );
 }
